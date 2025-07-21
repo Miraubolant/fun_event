6fdccd47cc12d5eeda0261ff8757884006a49a17
Miraubolant <143420936+Miraubolant@users.noreply.github.com>
2025-07-21 20:08:34 +0200

    Updated Catalog.tsx

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -134,6 +134,8 @@ const Catalog: React.FC = () => {
                     target="_blank"
                     rel="noopener noreferrer"
                     className="px-4 py-3 border-2 border-orange-500 text-orange-500 rounded-lg font-semibold hover:bg-orange-500 hover:text-white transition-all"
+                  >
+                    💬
                   </a>
                 </div>
               </div>
